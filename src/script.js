2bea27d0d43d8c4fdfb2476efa0a342cb8aeaccb
alanarama <117275477+alanarama@users.noreply.github.com>
2022-12-14 16:28:19 +0000

    Updated JS

--- src/script.js
+++ src/script.js
@@ -1,100 +1,105 @@
-let now = new Date();
+function formatDate(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
 
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-  "Sunday",
-];
-let day = days[now.getDay()];
-let hour = now.getHours();
-if (hour < 10) {
-  hour = `0${hour}`;
-}
-let mins = now.getMinutes();
-if (mins < 10) {
-  mins = `0${mins}`;
-}
-let displayTime = document.querySelector("#date");
-displayTime.innerHTML = `${day}, ${hour}:${mins}`;
-
-function showCity(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#text-input");
-  let apiKey = "fda3688b1db05987dd5d07c237aecfba";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showTemp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day} ${hours}:${minutes}`;
 }
 
-let searchForm = document.querySelector("#city-search");
-searchForm.addEventListener("submit", showCity);
-
-function showTemp(response) {
-  let temp = Math.round(response.data.main.temp);
-  let tempElement = document.querySelector("#temperature");
-  tempElement.innerHTML = `${temp}`;
-
-  let currentCity = document.querySelector("#city");
-  currentCity.innerHTML = `${response.data.name}`;
-
-  let summary = document.querySelector("#description");
-  summary.innerHTML = `${response.data.weather[0].description}`;
-
-  let windSpeed = Math.round(response.data.wind.speed);
-  let windSpeedElement = document.querySelector("#wind");
-  windSpeedElement.innerHTML = `Wind: ${windSpeed} m/s`;
-
-  let humidity = response.data.main.humidity;
+function displayTemperature(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
-  humidityElement.innerHTML = humidity;
-
+  let windElement = document.querySelector("#wind");
+  let dateElement = document.querySelector("#date");
   let iconElement = document.querySelector("#icon");
+
+  celsiusTemperature = response.data.main.temp;
+
+  temperatureElement.innerHTML = Math.round(celsiusTemperature);
+  cityElement.innerHTML = response.data.name;
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  humidityElement.innerHTML = response.data.main.humidity;
+  windElement.innerHTML = Math.round(response.data.wind.speed);
+  dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
-
-  celsiusTemperature = response.data.main.temp;
-}
-//get current location//
-function displayPosition(position) {
-  let apiKey = "9f1515c6d557b936bd2810b8784d57c8";
-  let lat = position.coords.latitude;
-  let lon = position.coords.longitude;
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(showTemp);
 }
 
-function showTempForCurrentLoc(event) {
-  navigator.geolocation.getCurrentPosition(displayPosition);
+function search(city) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
 }
 
-let celsiusTemperature = null;
-
-let currentLocation = document.querySelector("#current-loc-button");
-currentLocation.addEventListener("click", showTempForCurrentLoc);
+function handleSubmit(event) {
+  event.preventDefault();
+  let cityInputElement = document.querySelector("#text-input");
+  search(cityInputElement.value);
+}
 
 function displayFahrenheitTemperature(event) {
   event.preventDefault();
   let temperatureElement = document.querySelector("#temperature");
+
+  celsiusLink.classList.remove("active");
+  fahrenheitLink.classList.add("active");
   let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
   temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
-let fahrenheitLink = document.querySelector("#fahrenheit-link");
-fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
-
 function displayCelsiusTemperature(event) {
   event.preventDefault();
+  celsiusLink.classList.add("active");
+  fahrenheitLink.classList.remove("active");
   let temperatureElement = document.querySelector("#temperature");
   temperatureElement.innerHTML = Math.round(celsiusTemperature);
 }
 
+let celsiusTemperature = null;
+
+let form = document.querySelector("#city-search");
+form.addEventListener("submit", handleSubmit);
+
+let fahrenheitLink = document.querySelector("#fahrenheit-link");
+fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
+
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
-showTempForCurrentLoc();
+
+function getCurrentPosition(position) {
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let apiKey = "9f1515c6d557b936bd2810b8784d57c8";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayTemperature);
+}
+
+function showTempForCurrentLoc(event) {
+  navigator.geolocation.getCurrentPosition(getCurrentPosition);
+}
+
+let currentLocation = document.querySelector("#current-loc-button");
+currentLocation.addEventListener("click", showTempForCurrentLoc);
+
+search("London");
