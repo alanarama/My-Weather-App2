6f6dcc5eccbf873814bf2345efcc30f60ca0f682
alanarama <117275477+alanarama@users.noreply.github.com>
2022-12-14 20:28:30 +0000

    added forecast

--- src/script.js
+++ src/script.js
@@ -22,6 +22,39 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast(response) {
+  let forecast = response.data.daily;
+
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+
+  days.forEach(function (forecastDay) {
+    forecastHTML =
+      forecastHTML +
+      `
+<div class="col-2">
+<div class="weather-forecast-date">
+${forecastday.dt}
+</div>
+<img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="" width="36"/>
+<div class="weather-forecast-temperatures">
+<span class="weather-forecast-temperature-max"> ${forecastDay.temp.max}°</span>
+<span class="weather-forecast-temperature-min"> ${forecastDay.temp.min}°</span>
+</div>
+</div>
+  </div>`;
+  });
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+function getForecast(coordinates) {
+  console.log(coordinates);
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let apiUrl = `https://api.openweathermap.org/data/3.0/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apikey}&units=metric`;
+  console.log(apiUrl);
+  axios.get(apiUrl).then(displayForecast);
+}
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -44,6 +77,8 @@ function displayTemperature(response) {
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
+
+  getForecast(response.data.coord);
 }
 
 function search(city) {
